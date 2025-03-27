8b5304ccf1cbff60d0d60b2f56bbcd35da0324cf
Jose Vega <jvegar@uni.pe>
2025-03-27 11:32:19 -0500

    initial commit

---
+++ shloader.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+# https://github.com/Kaderovski/shloader
+# me@kaderovski.com
+set -Eeuo pipefail
+trap end_shloader SIGINT SIGTERM ERR EXIT RETURN
+tput civis
+
+
+# Loaders
+# EMOJIS
+emoji_hour=( 0.08 '🕛' '🕐' '🕑' '🕒' '🕓' '🕔' '🕕' '🕖' '🕗' '🕘' '🕙' '🕚')
+emoji_face=( 0.08 '😐' '😀' '😍' '🙄' '😒' '😨' '😡')
+emoji_earth=( 0.1 🌍 🌎 🌏 )
+emoji_moon=( 0.08 🌑 🌒 🌓 🌔 🌕 🌖 🌗 🌘 )
+emoji_orange_pulse=( 0.1 🔸 🔶 🟠 🟠 🔶 )
+emoji_blue_pulse=( 0.1 🔹 🔷 🔵 🔵 🔷 )
+emoji_blink=( 0.06 😐 😐 😐 😐 😐 😐 😐 😐 😐 😑 )
+emoji_camera=( 0.05 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📸 📷 📸 )
+emoji_sick=( 0.2 🤢 🤢 🤮 )
+emoji_monkey=( 0.2 🙉 🙈 🙊 🙈 )
+emoji_bomb=( 0.2 '💣   ' ' 💣  ' '  💣 ' '   💣' '   💣' '   💣' '   💣' '   💣' '   💥' '    ' '    ' )
+# ASCII
+ball=( 0.2 '(●)' '(⚬)')
+arrow=( 0.06 '↑' '↗' '→' '↘' '↓' '↙' '←' '↖')
+cym=( 0.1 '⊏' '⊓' '⊐' '⊔')
+x_plus=( 0.08 '×' '+')
+line=( 0.08 '☰' '☱' '☳' '☷' '☶' '☴')
+ball_wave=( 0.1 '𓃉𓃉𓃉' '𓃉𓃉∘' '𓃉∘°' '∘°∘' '°∘𓃉' '∘𓃉𓃉')
+old=( 0.07 '—' "\\" '|' '/' )
+dots=( 0.04 '⣾' '⣽' '⣻' '⢿' '⡿' '⣟' '⣯' '⣷' )
+dots2=( 0.04 '⠋' '⠙' '⠹' '⠸' '⠼' '⠴' '⠦' '⠧' '⠇' '⠏' )
+dots3=( 0.04 '⠋' '⠙' '⠚' '⠞' '⠖' '⠦' '⠴' '⠲' '⠳' '⠓' )
+dots4=( 0.04 '⠄' '⠆' '⠇' '⠋' '⠙' '⠸' '⠰' '⠠' '⠰' '⠸' '⠙' '⠋' '⠇' '⠆' )
+dots5=( 0.04 '⠋' '⠙' '⠚' '⠒' '⠂' '⠂' '⠒' '⠲' '⠴' '⠦' '⠖' '⠒' '⠐' '⠐' '⠒' '⠓' '⠋' )
+dots6=( 0.04 '⠁' '⠉' '⠙' '⠚' '⠒' '⠂' '⠂' '⠒' '⠲' '⠴' '⠤' '⠄' '⠄' '⠤' '⠴' '⠲' '⠒' '⠂' '⠂' '⠒' '⠚' '⠙' '⠉' '⠁' )
+dots7=( 0.04 '⠈' '⠉' '⠋' '⠓' '⠒' '⠐' '⠐' '⠒' '⠖' '⠦' '⠤' '⠠' '⠠' '⠤' '⠦' '⠖' '⠒' '⠐' '⠐' '⠒' '⠓' '⠋' '⠉' '⠈' )
+dots8=( 0.04 '⠁' '⠁' '⠉' '⠙' '⠚' '⠒' '⠂' '⠂' '⠒' '⠲' '⠴' '⠤' '⠄' '⠄' '⠤' '⠠' '⠠' '⠤' '⠦' '⠖' '⠒' '⠐' '⠐' '⠒' '⠓' '⠋' '⠉' '⠈' '⠈' )
+dots9=( 0.04  '⢹' '⢺' '⢼' '⣸' '⣇' '⡧' '⡗' '⡏' )
+dots10=( 0.04  '⢄' '⢂' '⢁' '⡁' '⡈' '⡐' '⡠' )
+dots11=( 0.04 '⠁' '⠂' '⠄' '⡀' '⢀' '⠠' '⠐' '⠈' )
+
+
+die() {
+  local code=${2-1}
+  exit "$code"
+}
+
+
+usage() {
+  cat <<EOF
+
+Available options:
+
+-h, --help            <OPTIONAL>    Print this help and exit
+-l, --loader          <OPTIONAL>   Chose loader to display
+-m, --message         <OPTIONAL>    Text to display while loading
+-e, --ending          <OPTIONAL>    Text to display when finishing
+
+EOF
+  exit 0
+}
+
+
+play_shloader() {
+  while true ; do
+    for frame in "${loader[@]}" ; do
+      printf "\r%s" "${frame} ${message}"
+      sleep "${speed}"
+    done
+  done
+}
+
+
+end_shloader() {
+  kill "${shloader_pid}" &>/dev/null
+  tput cnorm
+  if [[ "${ending}" ]]; then
+    printf "\r${ending}"; echo
+  fi
+}
+
+
+shloader() {
+  loader=''
+  message=''
+  ending=''
+
+  while :; do
+    case "${1-}" in
+    -h | --help) usage;;
+    -l | --loader)
+      loader="${2-}"
+      shift
+      ;;
+    -m | --message)
+      message="${2-}"
+      shift
+      ;;
+    -e | --ending)
+      ending="${2-}"
+      shift
+      ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  if [[ -z "${loader}" ]] ; then
+    loader=dots[@] 
+  else
+    loader=$loader[@]
+  fi
+
+  loader=( ${!loader} )
+  speed="${loader[0]}"
+  unset "loader[0]"
+
+  tput civis
+  play_shloader &
+  shloader_pid="${!}"
+}
+shloader
---
+++ yai.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Load shloader
+source /Users/josevega/repos/projects/sh-scripts/shloader.sh 2>/dev/null
+
+# Function to concatenate strings and evaluate the result
+main() {
+  # Input string
+  input_string=$1
+  
+  # Other strings to concatenate
+  raw_query="echo '$input_string' | fabric -p raw_query"
+
+  # Evaluate the concatenated string and color the output in cyan
+  eval "$raw_query" | while IFS= read -r line; do
+    echo -e "\033[36m${line}\033[0m"
+  done
+}
+
+
+# Check if an argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <input_string>"
+  exit 1
+fi
+
+# Call the function with the input string
+shloader -l dots2 -m "" -e "Done!" 2>/dev/null
+
+main "$1"
+
+end_shloader
